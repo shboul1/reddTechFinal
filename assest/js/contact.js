11ae9a91d39f29059c59ef6090001151c0c538b9
unknown <shboulv1@gmail.com>
2022-04-27 03:29:21 +0300

    trim video/ add pdf

--- assest/js/contact.js
+++ assest/js/contact.js
@@ -1,26 +1,25 @@
 function sendEmail() {
-    Email.send({
-        Host: "smtp.elasticemail.com",
-        Username: "shboulv1@gmail.com",
-        Password: "BE11316F0945AE534EE15491B0A187C83752",
-        To: "shboulv1@gmail.com",
-        From: document.getElementById('email').value,
-        Subject: "This is the subject",
-        Body: "And this is the body",
-    }).then((message) => alert(message));
+  Email.send({
+    Host: "smtp.elasticemail.com",
+    Username: "shboulv1@gmail.com",
+    Password: "BE11316F0945AE534EE15491B0A187C83752",
+    To: "shboulv1@gmail.com",
+    From: document.getElementById("email").value,
+    Subject: "This is the subject",
+    Body: "And this is the body",
+  }).then((message) => alert(message));
 }
 
 function navLinks() {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    burger.addEventListener("click", () => {
-      nav.classList.toggle("nav-active");
-      if(nav.classList.contains('nav-active')) {
-        burger.classList.add("opened")
-      } else {
-        burger.classList.remove("opened")
-      }
-    });
-    
-  }
-  navLinks();
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".nav-links");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("nav-active");
+    if (nav.classList.contains("nav-active")) {
+      burger.classList.add("opened");
+    } else {
+      burger.classList.remove("opened");
+    }
+  });
+}
+navLinks();
